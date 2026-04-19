f61f2a91e5c679967428595edb86090d1315deb2
AmanUndre <amanundre8@gmail.com>
2026-04-19 15:54:46 +0530

    change is db.ts,server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,11 +5,18 @@ import { checkDatabaseConnection } from './config/db.health.js';
 const PORT = process.env.PORT;
 
 async function startServer() {
-  await checkDatabaseConnection();
+  try {
+    await checkDatabaseConnection();
+    console.log("Database connected successfully");
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Startup failed:", error);
+    process.exit(1);
+  }
 }
 
 startServer();
